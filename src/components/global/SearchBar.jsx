51ac56281439f0e29d873e71c5b1c7b7b740697c
theobaidul <81848149+theobaidul@users.noreply.github.com>
2024-04-05 10:39:59 +0600

    search complete

--- src/components/global/SearchBar.jsx
+++ src/components/global/SearchBar.jsx
@@ -1,9 +1,54 @@
+import useDebounce from '@/hooks/useDebounce';
+import { useLazySearchProductsQuery } from '@/redux/features/products/productsApi';
+import { useEffect, useState } from 'react';
+import { useLocation } from 'react-router-dom';
+import SearchItem from './SearchItem';
+
 export default function SearchBar() {
+  const [searchTerm, setSearchTerm] = useState('');
+  const location = useLocation();
+  const debounce = useDebounce();
+  const [searchProducts, { data, isError, error }] =
+    useLazySearchProductsQuery();
+  const { products } = data || {};
+
+  const handleSearch = (event) => {
+    setSearchTerm(event.target.value);
+    const trimedValue = event?.target?.value?.trim();
+    debounce(() => searchProducts(trimedValue));
+  };
+
+  let content;
+  if (isError) {
+    content = <div>{error?.data?.message}</div>;
+  } else if (products?.length === 0) {
+    content = <div>No products found</div>;
+  } else if (products?.length > 0) {
+    content = products?.map((product) => (
+      <SearchItem key={product?.id} product={product} />
+    ));
+  } else {
+    content = <div>Loading...</div>;
+  }
+
+  useEffect(() => {
+    setSearchTerm('');
+  }, [location.key]);
+
   return (
-    <input
-      type="search"
-      placeholder="Search products..."
-      className="w-full max-w-96 rounded-md border px-2 py-1 outline-none"
-    />
+    <div className="relative w-96">
+      <input
+        type="search"
+        placeholder="Search products..."
+        className="w-full max-w-96 rounded-md border px-2 py-1 outline-none"
+        value={searchTerm}
+        onChange={handleSearch}
+      />
+      {searchTerm?.trim() && (
+        <div className="absolute top-14 min-h-20 w-full space-y-4 rounded-lg bg-gray-100 p-4">
+          {content}
+        </div>
+      )}
+    </div>
   );
 }
---
+++ src/components/global/SearchItem.jsx
@@ -0,0 +1,23 @@
+import { Link } from 'react-router-dom';
+import Stars from '../common/Stars';
+
+export default function SearchItem({ product }) {
+  const { id, title, price, rating, thumbnail } = product || {};
+
+  return (
+    <Link to={`/products/${id}`}>
+      <div className="mt-2 flex gap-4">
+        <img
+          src={thumbnail}
+          alt="result"
+          className="size-14 object-cover object-center"
+        />
+        <div className="flex flex-col">
+          <p className="font-bold">{title}</p>
+          <p className="text-xs text-gray-500">{price}</p>
+          <Stars rating={rating} />
+        </div>
+      </div>
+    </Link>
+  );
+}
